6e8e58cd4243d6197fc6f363c4648a74a17d0dab
zzibo <zibozzb@gmail.com>
2024-07-28 23:41:13 +0800

    change of photo

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: "https://echonotes-fe.onrender.com",
+  //origin: "http://localhost:3000",
   optionsSuccessStatus: 200,
 };
 
--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,37 +1,57 @@
-import React from 'react';
-import './AboutUs.css';
-import teamMember1Photo from '../Assets/teamMember1.jpg';
-import teamMember2Photo from '../Assets/teamMember2.jpg';
+import React from "react";
+import "./AboutUs.css";
+import teamMember1Photo from "../Assets/teamMember1.jpg";
+import teamMember2Photo from "../Assets/teamMember2.JPG";
 
 function AboutUs() {
   return (
     <div className="about-us">
       <h1>About Us</h1>
       <p className="intro-text">
-        Welcome to our About Us page! Our team consists of dedicated professionals who are passionate about technology and design.
+        Welcome to our About Us page! Our team consists of dedicated
+        professionals who are passionate about technology and design.
       </p>
 
       <div className="team-member">
-        <img src={teamMember1Photo} alt="Team Member 1" className="team-photo" />
+        <img
+          src={teamMember1Photo}
+          alt="Team Member 1"
+          className="team-photo-a"
+        />
         <div className="member-info">
           <h2>Aayush</h2>
-          <p className='info'>Aayush is a Year 1 Business Analytics student at NUS</p>
+          <p className="info">
+            Aayush is a Year 1 Business Analytics student at NUS
+          </p>
         </div>
       </div>
 
       <div className="team-member">
-        <img src={teamMember2Photo} alt="Team Member 2" className="team-photo" />
+        <img
+          src={teamMember2Photo}
+          alt="Team Member 2"
+          className="team-photo-z"
+        />
         <div className="member-info">
           <h2>Zibo</h2>
-          <p className='info'>Zibo is a Year 1 Computer Science student at NUS</p>
+          <p className="info">
+            Zibo is a Year 1 Computer Science student at NUS
+          </p>
         </div>
       </div>
 
       <div className="additional-info">
         <h2>Our Mission</h2>
-              <p className="placeholder-text">
-              Our mission is to revolutionize the way students learn by developing an app that allows students to easily produce their own study material. By leveraging techniques such as active recall and spaced repetition, we aim to enhance learning efficiency and engagement. We are committed to providing an enjoyable and gamified learning experience, complete with networking features and incentives, to encourage regular study and make learning more accessible and effective for all students.
-              </p>
+        <p className="placeholder-text">
+          Our mission is to revolutionize the way students learn by developing
+          an app that allows students to easily produce their own study
+          material. By leveraging techniques such as active recall and spaced
+          repetition, we aim to enhance learning efficiency and engagement. We
+          are committed to providing an enjoyable and gamified learning
+          experience, complete with networking features and incentives, to
+          encourage regular study and make learning more accessible and
+          effective for all students.
+        </p>
       </div>
     </div>
   );
